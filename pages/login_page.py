f2e4273fadb574545fc9a915dbabe25fdc9a6c10
adrianamarcelaorozco <aorozco@ext.globalseguros.co>
2026-06-16 18:01:46 -0500

    terminacion de login

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,16 +1,38 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
+
 class LoginPage(BasePage):
+
+    URL = "http://10.1.2.33/PortalEM/ingreso-usuarios.aspx"
+
     # Locators
     BTN_CANCELAR = (By.XPATH, "//input[@value='Cancelar']")
     INPUT_USUARIO = (By.ID, "usuarioLogin")
-    INPUT_PASS = (By.ID, "claveLogin")
+    INPUT_PASSWORD = (By.ID, "claveLogin")
+    BTN_INGRESAR = (By.ID, "btnLogin")
+    BTN_MIS_COTIZACIONES = (By.ID, "IrCotizadores")
 
     def open(self):
-        self.driver.get("http://10.1.2.33/PortalEM/ingreso-usuarios.aspx")
+        self.driver.get(self.URL)
 
-    def login_workflow(self, user, pwd):
+    def click_cancelar(self):
         self.click(self.BTN_CANCELAR)
-        self.type_text(self.INPUT_USUARIO, user)
-        self.type_text(self.INPUT_PASS, pwd)
+
+    def enter_username(self, username):
+        self.type_text(self.INPUT_USUARIO, username)
+
+    def enter_password(self, password):
+        self.type_text(self.INPUT_PASSWORD, password)
+
+    def click_ingresar(self):
+        self.click(self.BTN_INGRESAR)
+
+    def click_mis_cotizaciones(self):
+        self.click(self.BTN_MIS_COTIZACIONES)
+
+    def login(self, username, password):
+        self.click_cancelar()
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_ingresar()
--- test/test_login.py
+++ test/test_login.py
@@ -1,9 +1,26 @@
-import time 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pages.login_page import LoginPage
 
-def test_login_exitoso(driver):
+
+def test_ingresar_a_mis_cotizaciones(driver):
+
     login = LoginPage(driver)
+
     login.open()
-    login.login_workflow("NCRUZV", "Clave123")
-    
-    time.sleep(5)
+    login.login("NCRUZV", "Clave123")
+
+    # Entrar al iframe
+    WebDriverWait(driver, 20).until(
+        EC.frame_to_be_available_and_switch_to_it((By.ID, "contenido"))
+    )
+
+    # Clic en Mis cotizaciones
+    boton = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "IrCotizadores"))
+    )
+
+    boton.click()
+
+    print("Click en Mis cotizaciones realizado")
